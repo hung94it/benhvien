2ab8cc24c78c5dec549834374dbbee255a804180
finaluqb <finaluqb@gmail.com>
2012-12-08 19:15:02 +0000

    Báo lỗi thiếu reference thì vào đây down https://code.google.com/p/accord/downloads/detail?name=Accord.NET%20Framework-2.8.0.exe kiểm tra đường dẫn mấy cái reference coi đúng chưa C:\Program Files (x86)\Accord.NET\Framework\Release

---
+++ Diabetes/Project/DiabetesDido/Form/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DiabetesDido
+{
+    public partial class Form2 : Form
+    {
+        //private DiabetesDataSetTableAdapters.DatasourceTableAdapter datasourceTableAdapter;
+
+        public Form2()
+        {
+            InitializeComponent();
+
+            //this.datasourceTableAdapter = new DiabetesDido.DiabetesDataSetTableAdapters.DatasourceTableAdapter();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'diabetesDataSet.Datasource' table. You can move, or remove it, as needed.
+            this.datasourceTableAdapter.Fill(this.diabetesDataSet.Datasource);
+
+
+        }
+    }
+}
